826e64185d19885f75fe5dd29604271d49c2e347
NoZ915 <tina0915tw0915@gmail.com>
2022-11-16 14:44:28 +0800

    added: statue animate

--- game/main.js
+++ game/main.js
@@ -7,9 +7,11 @@ let Application = PIXI.Application,
   Sprite = PIXI.Sprite,
   Text = PIXI.Text,
   TextStyle = PIXI.TextStyle;
-var playerSheet = {};
 
-var state, player, scene1, scene2, block, block2, gameScene,
+var playerSheet = {};
+var statueSheet = {};
+
+var state, player, scene1, scene2, statue, blo2, gameScene,
   playerContainer, scene1Container, scene2Container;
 
 let app = new Application({
@@ -26,10 +28,11 @@ mainCanvas.appendChild(app.view);
 loader
   .add("scene1", "./img/scene1.png")
   .add("scene2", "./img/scene2.png")
-  .add("player", "./img/player.png")
-  .add("playerAnimate", "./img/playerAnimate.png")
-  .add("block", "./img/block.png")
-  .add("block2", "./img/block2.png")
+  .add("player", "./img/player/player.png")
+  .add("playerAnimate", "./img/player/playerAnimate.png")
+  .add("statue", "./img/statue/statue.png")
+  .add("statueAnimate", "/img/statue/statueAnimate.png")
+  .add("blo2", "./img/blo2.png")
   .load(setUp);
 
 function setUp() {
@@ -44,24 +47,27 @@ function setUp() {
   scene1.width = app.screen.width * 2;
   scene1.height = app.screen.height * 3;
   scene1Container.addChild(scene1);
-  block = new Sprite(resources.block.texture);
-  block.x = app.screen.width * 0.5 + 150;
-  block.y = app.screen.height * 0.5;
-  block.width = 32;
-  block.height = 32;
-  scene1Container.addChild(block);
+
+  createStatueSheet();
+  createStatue();
+  // statue = new Sprite(resources.statue.texture);
+  // statue.x = app.screen.width * 0.5 + 150;
+  // statue.y = app.screen.height * 0.5;
+  // statue.width = 512;
+  // statue.height = 486;
+  // scene1Container.addChild(statue);
 
   app.stage.addChild(scene2Container);
   scene2 = new Sprite(resources.scene2.texture);
   scene2.width = app.screen.width * 4;
   scene2.height = app.screen.height * 4;
   scene2Container.addChild(scene2);
-  block2 = new Sprite(resources.block2.texture);
-  block2.x = app.screen.width * 0.5 + 180;
-  block2.y = app.screen.height * 0.5;
-  block2.width = 32;
-  block2.height = 32;
-  scene2Container.addChild(block2);
+  blo2 = new Sprite(resources.blo2.texture);
+  blo2.x = app.screen.width * 0.5 + 180;
+  blo2.y = app.screen.height * 0.5;
+  blo2.width = 32;
+  blo2.height = 32;
+  scene2Container.addChild(blo2);
   scene2Container.visible = false;
 
   createPlayerSheet();
@@ -69,51 +75,51 @@ function setUp() {
 
   //Capture the keyboard arrow keys
   let left = keyboard(37),
-      up = keyboard(38),
-      right = keyboard(39),
-      down = keyboard(40);
+    up = keyboard(38),
+    right = keyboard(39),
+    down = keyboard(40);
 
   left.press = function () {
-    keyboardPress(playerSheet.walkLeft);
+    turnOnAnimate(player, playerSheet.walkLeft);
     player.vx = -5;
     player.vy = 0;
   };
   left.release = function () {
     if (!right.isDown && player.vy === 0) {
-      keyboardRelease(playerSheet.standLeft);
+      turnOffAnimate(player, playerSheet.standLeft);
       player.vx = 0;
     }
   };
   up.press = function () {
-    keyboardPress(playerSheet.walkUp);
+    turnOnAnimate(player, playerSheet.walkUp);
     player.vy = -5;
     player.vx = 0;
   };
   up.release = function () {
     if (!down.isDown && player.vx === 0) {
-      keyboardRelease(playerSheet.standUp);
+      turnOffAnimate(player, playerSheet.standUp);
       player.vy = 0;
     }
   };
   right.press = function () {
-    keyboardPress(playerSheet.walkRight);
+    turnOnAnimate(player, playerSheet.walkRight);
     player.vx = 5;
     player.vy = 0;
   };
   right.release = function () {
     if (!left.isDown && player.vy === 0) {
-      keyboardRelease(playerSheet.standRight);
-    player.vx = 0;
+      turnOffAnimate(player, playerSheet.standRight);
+      player.vx = 0;
     }
   };
   down.press = function () {
-    keyboardPress(playerSheet.walkDown);
+    turnOnAnimate(player, playerSheet.walkDown);
     player.vy = 5;
     player.vx = 0;
   };
   down.release = function () {
     if (!up.isDown && player.vx === 0) {
-      keyboardRelease(playerSheet.standDown);
+      turnOffAnimate(player, playerSheet.standDown);
       player.vy = 0;
     }
   };
@@ -134,11 +140,21 @@ function play(delta) {
 
   if (scene1Container.visible) {
     sceneLimit(player, playerContainer, scene1, scene1Container, app);
-    if (hitTestRectangle(player, block)) {
-      scene1Container.visible = false;
-      scene2Container.visible = true;
-      state = goToScene2;
-      console.log("hit 1");
+    if (hitTestRectangle(player, statue)) {
+    
+      if(!statue.playing){
+        turnOnAnimate(statue, statueSheet.on);
+      }
+
+      let space = keyboard(32);
+      space.press = function () {
+        scene1Container.visible = false;
+        scene2Container.visible = true;
+        state = goToScene2;
+        console.log("hit 1");
+      }
+    }else{
+      turnOffAnimate(statue, statueSheet.off);
     }
   }
 }
@@ -151,7 +167,7 @@ function goToScene2() {
 
   if (scene2Container.visible) {
     sceneLimit(player, playerContainer, scene2, scene2Container, app);
-    if (hitTestRectangle(player, block2)) {
+    if (hitTestRectangle(player, blo2)) {
       scene2Container.visible = false;
       scene1Container.visible = true;
       state = play;
@@ -160,56 +176,55 @@ function goToScene2() {
   }
 }
 
-function createPlayerSheet(){
-  let ssheet = PIXI.Texture.from(resources.playerAnimate.texture);
+function createPlayerSheet() {
+  let psheet = PIXI.Texture.from(resources.playerAnimate.texture);
   let w = 100;
   let h = 100;
-  
-  playerSheet["standDown"]=[
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(4*w, 3*h, w, h))
+
+  playerSheet["standDown"] = [
+    new PIXI.Texture(psheet, new PIXI.Rectangle(4 * w, 3 * h, w, h))
   ];
-  playerSheet["standUp"]=[
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(0*w, 3*h, w, h))
+  playerSheet["standUp"] = [
+    new PIXI.Texture(psheet, new PIXI.Rectangle(0 * w, 3 * h, w, h))
   ];
-  playerSheet["standLeft"]=[
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(4*w, 0*h, w, h))
+  playerSheet["standLeft"] = [
+    new PIXI.Texture(psheet, new PIXI.Rectangle(4 * w, 0 * h, w, h))
   ];
-  playerSheet["standRight"]=[
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(0*w, 2*h, w, h))
+  playerSheet["standRight"] = [
+    new PIXI.Texture(psheet, new PIXI.Rectangle(0 * w, 2 * h, w, h))
   ];
-  playerSheet["walkDown"]=[
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(0*w, 0*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(3*w, 3*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(4*w, 3*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(0*w, 4*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(1*w, 4*h, w, h))
+  playerSheet["walkDown"] = [
+    new PIXI.Texture(psheet, new PIXI.Rectangle(0 * w, 0 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(3 * w, 3 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(4 * w, 3 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(0 * w, 4 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(1 * w, 4 * h, w, h))
   ];
-  playerSheet["walkUp"]=[
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(3*w, 2*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(4*w, 2*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(0*w, 3*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(1*w, 3*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(2*w, 3*h, w, h))
+  playerSheet["walkUp"] = [
+    new PIXI.Texture(psheet, new PIXI.Rectangle(3 * w, 2 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(4 * w, 2 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(0 * w, 3 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(1 * w, 3 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(2 * w, 3 * h, w, h))
   ];
-  playerSheet["walkLeft"]=[
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(1*w, 0*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(2*w, 0*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(3*w, 0*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(4*w, 0*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(0*w, 1*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(1*w, 1*h, w, h))
+  playerSheet["walkLeft"] = [
+    new PIXI.Texture(psheet, new PIXI.Rectangle(1 * w, 0 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(2 * w, 0 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(3 * w, 0 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(4 * w, 0 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(0 * w, 1 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(1 * w, 1 * h, w, h))
   ];
-  playerSheet["walkRight"]=[
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(2*w, 1*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(3*w, 1*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(4*w, 1*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(0*w, 2*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(1*w, 2*h, w, h)),
-    new PIXI.Texture(ssheet, new PIXI.Rectangle(2*w, 2*h, w, h))
+  playerSheet["walkRight"] = [
+    new PIXI.Texture(psheet, new PIXI.Rectangle(2 * w, 1 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(3 * w, 1 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(4 * w, 1 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(0 * w, 2 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(1 * w, 2 * h, w, h)),
+    new PIXI.Texture(psheet, new PIXI.Rectangle(2 * w, 2 * h, w, h))
   ];
 }
-
-function createPlayer(){
+function createPlayer() {
   player = new PIXI.extras.AnimatedSprite(playerSheet.standDown);
   player.anchor.set(0.5, 0.5);
   player.animationSpeed = 0.3;
@@ -224,12 +239,40 @@ function createPlayer(){
   player.play();
 }
 
-function keyboardPress(animateState){
-  player.textures = animateState;
-  player.loop = true;
-  player.play();
+function createStatueSheet(){
+  let ssheet = PIXI.Texture.from(resources.statueAnimate.texture);
+  let w = 512;
+  let h = 486;
+
+  statueSheet["on"] = [
+    new PIXI.Texture(ssheet, new PIXI.Rectangle(0 * w, 0 * h, w, h)),
+    new PIXI.Texture(ssheet, new PIXI.Rectangle(1 * w, 0 * h, w, h)),
+    new PIXI.Texture(ssheet, new PIXI.Rectangle(0 * w, 1 * h, w, h)),
+    new PIXI.Texture(ssheet, new PIXI.Rectangle(1 * w, 1 * h, w ,h))
+  ];
+  statueSheet["off"] = [
+    new PIXI.Texture(ssheet, new PIXI.Rectangle(0*w, 0*h, w, h))
+  ];
 }
-function keyboardRelease(animateState){
-  player.loop = false;
-  player.textures = animateState;
+function createStatue(){
+  statue = new PIXI.extras.AnimatedSprite(statueSheet.off);
+  statue.anchor.set(0.5, 0.5);
+  statue.animationSpeed = 0.3;
+  statue.loop = false;
+  statue.x = app.screen.width * 0.5 + 150;
+  statue.y = app.screen.height * 0.5;
+  statue.width = 512;
+  statue.height = 486;
+  scene1Container.addChild(statue);
+  statue.play();
+}
+
+function turnOnAnimate(sprite, animateState) {
+  sprite.textures = animateState;
+  sprite.loop = true;
+  sprite.play();
+}
+function turnOffAnimate(sprite, animateState) {
+  sprite.loop = false;
+  sprite.textures = animateState;
 }
